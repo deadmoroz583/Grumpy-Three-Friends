484f806d66457edfbb6482d4bf6c31b61ffaafb9
Lofetty <Lofetty@yandex.ru>
2018-12-12 18:58:28 +0700

    228

--- Unity_project/Grumpy-Three-Friends/Assets/Scripts/GlobalNames.cs
+++ Unity_project/Grumpy-Three-Friends/Assets/Scripts/GlobalNames.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GlobalNames : MonoBehaviour
 {
 	public static double soundVolume;
+	public static int[] ach = {0, 0, 0, 0, 0, 0, 0};
+	public RawImage[] achimg = new RawImage[7];
+	public Texture m_Texture;
 	
 	void Start()
 	{
@@ -13,6 +19,15 @@ public class GlobalNames : MonoBehaviour
 	
 	void Update()
 	{
-		
+		if (SceneManager.GetActiveScene().name == "Main_Menu")
+		{
+			for (int i = 0; i <= 6; i++)
+			{
+				if (ach[i] == 1)
+				{
+					achimg[i].texture = m_Texture;
+				}
+			}
+		}
 	}
 }
